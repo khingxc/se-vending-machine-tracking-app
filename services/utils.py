d8d47439a6d96b7e9dc478d4a735ade6a79ad29a
khingxc <khingkhwanc@gmail.com>
2023-01-19 14:41:07 +0700

    fix: shortening filter list function

--- services/utils.py
+++ services/utils.py
@@ -11,19 +11,12 @@ class Utils:
         self.db = db
 
     def filter_list(data, machine_id=0):
-        result = []
         match data.lower():
             case "machine":
                 machines = VendingMachine.query.order_by(VendingMachine.id)
-                for m in machines:
-                    result.append(m)
+                return [m for m in machines]
             case "stock":
                 stocks = Stock.query.filter(Stock.machine_id == int(machine_id))
-                if stocks is None:
-                    pass
-                else:
-                    for s in stocks:
-                        result.append(s)
-            case _:
-                pass
-        return result
+                if stocks is not None:
+                    return [s for s in stocks]
+        return []
