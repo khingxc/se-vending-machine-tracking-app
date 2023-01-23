9046c869a326c76dede85572a030d060a0b7206a
khingxc <khingkhwanc@gmail.com>
2023-01-24 01:43:59 +0700

    fix: running black

--- tests/routes_vending_machine/delete.py
+++ tests/routes_vending_machine/delete.py
@@ -10,7 +10,6 @@ local_host_address = os.environ["LOCALHOST_ADDR"]
 
 
 class TestDeleteMachine(unittest.TestCase):
-
     def test_delete_machine_success(self):
         created_machine_id = create.TestCreateMachine().test_create_machine_success()
         delete_machine_url = f"{local_host_address}/machine/{created_machine_id}/delete"
@@ -18,12 +17,5 @@ class TestDeleteMachine(unittest.TestCase):
         assert response.status_code == 204
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
-
-
-
-
-
-
